12c2f0b7623692986a3bb6e2472a22fb079c04a0
Adam Escobedo <acescobedo@smu.edu>
2022-05-23 05:14:58 -0500

    created Sieve class and added custom linked list from previous project

--- Sieve.h
+++ Sieve.h
@@ -5,4 +5,22 @@
 #ifndef SIEVE_OF_ERATOSTHENES_SIEVE_H
 #define SIEVE_OF_ERATOSTHENES_SIEVE_H
 
+#include <unordered_map>
+#include "DLList/DLList.h"
+
+class Sieve{
+private:
+    std::unordered_map<int, Node<int> *> nodeMap;
+    DLList<int> data;
+public:
+    explicit Sieve(int size);
+};
+
+Sieve::Sieve(int size) {
+    for (int i = 2; i < size; ++i) {
+        this->data.push_back(i);
+        this->nodeMap[i] = this->data.last().ptr;
+    }
+}
+
 #endif //SIEVE_OF_ERATOSTHENES_SIEVE_H
